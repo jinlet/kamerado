f12e24838af8ff5a0e359451532686d4e0a6b963
jinlet <jinletr@gmail.com>
2024-05-21 22:32:11 +0200

    Added more design and a burger menu on smaller screens

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Active for FAQ content //
 const faqs = document.querySelectorAll(".faq-content");
 
 faqs.forEach(faq => {
@@ -5,3 +6,13 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     });
 });
+
+//Active for Burger-menu //
+const hamMenu = document.querySelector(".burger-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+})
