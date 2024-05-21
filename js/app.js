1c392ab95fba89f357c59f2f57b0d7c6df6df121
jinlet <jinletr@gmail.com>
2024-05-21 22:53:08 +0200

    Small bugfix with burger-nav-bar

--- js/app.js
+++ js/app.js
@@ -16,3 +16,11 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
 })
+
+// Function to close the menu if screen width exceeds 1200px
+function closeMenuOnLargeScreen() {
+    const menu = document.querySelector('.off-screen-menu');
+    if (window.innerWidth >= 1200 && menu.classList.contains('active')) {
+        menu.classList.remove('active');
+    }
+}
